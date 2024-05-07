9eecded06fa380d300d8f152a0349024dcfcebe8
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-05-08 03:49:46 +0700

    Menambahkan fitur untuk mengambil data dari API

--- lib/Halaman_Utama.dart
+++ lib/Halaman_Utama.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HalamanToko extends StatefulWidget {
   const HalamanToko({super.key});
@@ -8,11 +11,36 @@ class HalamanToko extends StatefulWidget {
 }
 
 class _HalamanTokoState extends State<HalamanToko> {
+
+  List _listdata = [];
+  bool _loading = true;
+
+  Future _getdata()async{
+    try {
+      final respon =
+          await http.get(Uri.parse('http://192.168.100.6/api_pam/read.php'));
+          if (respon.statusCode == 200) {
+        final data = jsonDecode(respon.body);
+        setState(() {
+          _listdata = data;
+          _loading = false;
+        });
+      }
+
+    } catch (e) {
+      print(e);
+    }
+  }
+  void initState() {
+    _getdata();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tambah Toko'),
+        title: Text('Halaman Toko'),
         backgroundColor: Colors.deepOrange,
       ),
     );
