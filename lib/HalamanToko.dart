f8dd424f216418a7cfc426543c8550e0f6daf61d
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-05-08 04:55:01 +0700

    Menambahkan fungsi untuk menyimpan data toko baru

--- lib/HalamanToko.dart
+++ lib/HalamanToko.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class TambahToko extends StatefulWidget {
   const TambahToko({super.key});
@@ -8,6 +9,20 @@ class TambahToko extends StatefulWidget {
 }
 
 class _TambahTokoState extends State<TambahToko> {
+  final formKey = GlobalKey<FormState>();
+  TextEditingController nama_toko = TextEditingController();
+
+  Future _simpan() async {
+    final respon = await http
+        .post(Uri.parse('http://192.168.100.6/api_pam/create.php'), body: {
+      'nama_toko': nama_toko.text,
+    });
+    if (respon.statusCode == 200) {
+      return true;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
